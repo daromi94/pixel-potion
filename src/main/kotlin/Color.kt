80accc89928d3dfee17e6c30e163dcbd2f67362a
daromi94 <davromero07@gmail.com>
2024-07-21 17:08:07 -0500

    feat: :sparkles: make single color image

---
+++ src/main/kotlin/Color.kt
@@ -0,0 +1,21 @@
+package com.pixel.potion
+
+class Color(
+    private val red: Int,
+    private val green: Int,
+    private val blue: Int,
+) {
+    init {
+        if (this.red !in 0..0xFF) {
+            throw IllegalArgumentException("red channel must be between 0 and 255")
+        }
+        if (this.green !in 0..0xFF) {
+            throw IllegalArgumentException("green channel must be between 0 and 255")
+        }
+        if (this.blue !in 0..0xFF) {
+            throw IllegalArgumentException("blue channel must be between 0 and 255")
+        }
+    }
+
+    fun toInt(): Int = (this.red shl 16) or (this.green shl 8) or this.blue
+}
---
+++ src/main/kotlin/Image.kt
@@ -0,0 +1,15 @@
+package com.pixel.potion
+
+import java.awt.image.BufferedImage
+
+fun makeImage(
+    width: Int,
+    height: Int,
+    color: Color,
+): BufferedImage {
+    val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    val pixel = color.toInt()
+    val pixels = IntArray(width * height) { pixel }
+    image.setRGB(0, 0, width, height, pixels, 0, width)
+    return image
+}
