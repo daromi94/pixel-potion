fc8548b7f3731ce12bf4561fb1df24be281e10e9
daromi94 <davromero07@gmail.com>
2024-07-21 21:26:59 -0500

    feat: :sparkles: convert hex value to color

--- src/main/kotlin/Color.kt
+++ src/main/kotlin/Color.kt
@@ -6,20 +6,26 @@ class Color(
     private val blue: Int,
 ) {
     init {
-        if (this.red !in 0x00..0xFF) {
-            throw IllegalArgumentException("red channel must be between 0 and 255")
-        }
-        if (this.green !in 0x00..0xFF) {
-            throw IllegalArgumentException("green channel must be between 0 and 255")
-        }
-        if (this.blue !in 0x00..0xFF) {
-            throw IllegalArgumentException("blue channel must be between 0 and 255")
+        when {
+            this.red !in 0x00..0xFF -> throw IllegalArgumentException("red channel must be in range [0, 255]")
+
+            this.green !in 0x00..0xFF -> throw IllegalArgumentException("green channel must be in range [0, 255]")
+
+            this.blue !in 0x00..0xFF -> throw IllegalArgumentException("blue channel must be in range [0, 255]")
         }
     }
 
     companion object {
         val WHITE = Color(0xFF, 0xFF, 0xFF)
         val BLACK = Color(0x00, 0x00, 0x00)
+
+        @JvmStatic
+        fun fromHex(value: Int): Color {
+            val red = (value and 0xFF0000) shr 16
+            val green = (value and 0xFF00) shr 8
+            val blue = value and 0xFF
+            return Color(red, green, blue)
+        }
     }
 
     fun toInt(): Int = (this.red shl 16) or (this.green shl 8) or this.blue
