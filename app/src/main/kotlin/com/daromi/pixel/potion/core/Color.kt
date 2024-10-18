ee09d6ab809771eadce3ceba578207b175814693
daromi94 <davromero07@gmail.com>
2024-10-17 21:06:15 -0500

    refactor: :recycle: compact channel

--- app/src/main/kotlin/com/daromi/pixel/potion/core/Color.kt
+++ app/src/main/kotlin/com/daromi/pixel/potion/core/Color.kt
@@ -1,34 +1,40 @@
 package com.daromi.pixel.potion.core
 
-data class Color(
-    val red: Channel,
-    val green: Channel,
-    val blue: Channel,
-) {
-    companion object {
-        val WHITE = Color(Channel.MAX, Channel.MAX, Channel.MAX)
-        val BLACK = Color(Channel.MIN, Channel.MIN, Channel.MIN)
+data class Color(val red: Channel, val green: Channel, val blue: Channel) {
 
-        fun from(value: Int): Color? {
-            val red = Channel.from((value and 0xFF0000) shr 16) ?: return null
-            val green = Channel.from((value and 0x00FF00) shr 8) ?: return null
-            val blue = Channel.from(value and 0x0000FF) ?: return null
+  companion object {
+    val WHITE = Color(Channel.MAX, Channel.MAX, Channel.MAX)
+    val BLACK = Color(Channel.MIN, Channel.MIN, Channel.MIN)
 
-            return Color(red, green, blue)
-        }
+    fun from(value: UInt): Color {
+      val red   = Channel.compact(value and 0xFF0000u shr 16)
+      val green = Channel.compact(value and 0x00FF00u shr 8)
+      val blue  = Channel.compact(value and 0x0000FFu)
+
+      return Color(red, green, blue)
     }
+  }
 
-    fun toInt(): Int = (this.red.value shl 16) or (this.green.value shl 8) or this.blue.value
+  fun toUInt(): UInt {
+    val high = this.red.expand() shl 16
+    val mid  = this.green.expand() shl 8
+    val low  = this.blue.expand()
+
+    return high or mid or low
+  }
 }
 
 @JvmInline
-value class Channel(
-    val value: Int,
-) {
-    companion object {
-        val MIN = Channel(0x00)
-        val MAX = Channel(0xFF)
+value class Channel(val value: UByte) {
 
-        fun from(value: Int): Channel? = if (value !in (MIN.value)..(MAX.value)) null else Channel(value)
-    }
+  companion object {
+    val MIN = Channel(0x00u)
+    val MAX = Channel(0xFFu)
+
+    fun compact(value: UInt): Channel = Channel(value.toUByte())
+  }
+
+  fun expand(): UInt = this.value.toUInt()
+
+  override fun toString(): String = TODO("Not yet implemented!")
 }
