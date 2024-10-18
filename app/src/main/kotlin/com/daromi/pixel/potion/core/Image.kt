ecd0b1887589d9c0f35213ab118acbc19c5e6bed
daromi94 <davromero07@gmail.com>
2024-10-17 21:39:03 -0500

    refactor: :recycle: use unsigned type for image width and height

--- app/src/main/kotlin/com/daromi/pixel/potion/core/Image.kt
+++ app/src/main/kotlin/com/daromi/pixel/potion/core/Image.kt
@@ -1,50 +1,54 @@
 package com.daromi.pixel.potion.core
 
+import com.daromi.pixel.potion.util.not
 import java.awt.image.BufferedImage
 import java.io.InputStream
 import java.io.OutputStream
 import javax.imageio.ImageIO
 
-class Image private constructor(
-    private val buffer: BufferedImage,
-) {
-    companion object {
-        fun read(input: InputStream): Image {
-            val buffer = ImageIO.read(input)
+class Image private constructor(private val buffer: BufferedImage) {
 
-            return Image(buffer)
-        }
+  companion object {
+    fun read(input: InputStream): Image {
+      val buffer = ImageIO.read(input)
+
+      return Image(buffer)
+    }
+  }
+
+  val width: UInt
+    get() = this.buffer.width.toUInt()
+
+  val height: UInt
+    get() = this.buffer.height.toUInt()
+
+  fun crop(
+      x: UInt,
+      y: UInt,
+      width: UInt,
+      height: UInt,
+  ): Image? {
+    if (x + width > this.width || y + height > this.height) {
+      return null
     }
 
-    val width: Int get() = this.buffer.width
+    val area   = !(width * height)
+    val pixels = IntArray(area)
 
-    val height: Int get() = this.buffer.height
-
-    fun write(output: OutputStream) {
-        ImageIO.write(this.buffer, "JPG", output)
+    for (j in y..<y + height) {
+      for (i in x..<x + width) {
+        val index = (j - y) * width + (i - x)
+        pixels[!index] = this.buffer.getRGB(!i, !j)
+      }
     }
 
-    fun crop(
-        x: Int,
-        y: Int,
-        width: Int,
-        height: Int,
-    ): Image? {
-        if (x < 0 || width < 0 || x + width > this.width || y < 0 || height < 0 || y + height > this.height) {
-            return null
-        }
+    val buffer = BufferedImage(!width, !height, this.buffer.type)
+    buffer.setRGB(0, 0, !width, !height, pixels, 0, !width)
 
-        val pixels = IntArray(width * height)
+    return Image(buffer)
+  }
 
-        for (j in y..<y + height) {
-            for (i in x..<x + width) {
-                pixels[(j - y) * width + (i - x)] = this.buffer.getRGB(i, j)
-            }
-        }
-
-        val buffer = BufferedImage(width, height, this.buffer.type)
-        buffer.setRGB(0, 0, width, height, pixels, 0, width)
-
-        return Image(buffer)
-    }
+  fun write(output: OutputStream) {
+    ImageIO.write(this.buffer, "JPG", output)
+  }
 }
---
+++ app/src/main/kotlin/com/daromi/pixel/potion/util/UnsignedExtensions.kt
@@ -0,0 +1,3 @@
+package com.daromi.pixel.potion.util
+
+operator fun UInt.not(): Int = this.toInt()
